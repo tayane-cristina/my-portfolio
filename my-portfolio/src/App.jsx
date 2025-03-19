cfb47f4440c04ef661785f40dbbabce17aaa2759
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2025-03-19 00:12:38 -0300

    Criadas as rotas do navbar

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,32 @@
-
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/about/About'
+import Skills from './pages/skills/Skills'
+import Projects from './pages/projects/Projects'
+import Contact from './pages/contact/Contact'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <div>
-      <h1>Meu portfólio</h1>
-    </div>
+
+    <Router>
+      <div>
+        <Header />
+        <h1>Meu portfólio</h1>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+
   )
 }
 
